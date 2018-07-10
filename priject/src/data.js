4eb399c2d05f6a73ad0b6270f805082d178f7d70
Alex-135-135 <olecsij-andrijchuk@ukr.net>
2018-07-10 17:45:26 +0300

    project

---
+++ priject/src/data.js
@@ -0,0 +1,85 @@
+export const data =  [{
+  key: '1',
+  name: 'Olecsiy Andriychuk',
+  age: 18,
+  address: 'Zhytomir',
+  task: '1',
+  description: 'Task description',
+}, {
+  key: '2',
+  name: 'Olecsiy Andriychuk',
+  age: 18,
+  address: 'Zhytomir',
+  task: '2',
+  description: 'Task description',
+}, {
+  key: '3',
+  name: 'Olecsiy Andriychuk',
+  age: 18,
+  address: 'Zhytomir',
+  task: '3',
+  description: 'Task description',
+}, {
+  key: '4',
+  name: 'Olecsiy Andriychuk',
+  age: 18,
+  address: 'Zhytomir',
+  task: '4',
+  description: 'Task description',
+}, {
+  key: '5',
+  name: 'Olecsiy Andriychuk',
+  age: 18,
+  address: 'Zhytomir',
+  task: '5',
+  description: 'Task description',
+}, {
+  key: '6',
+  name: 'Olecsiy Andriychuk',
+  age: 18,
+  address: 'Zhytomir',
+  description: 'Task description',
+}, {
+  key: '7',
+  name: 'Olecsiy Andriychuk',
+  age: 18,
+  address: 'Zhytomir',
+  task: '7',
+  description: 'Task description',
+}, {
+  key: '8',
+  name: 'Olecsiy Andriychuk',
+  age: 18,
+  address: 'Zhytomir',
+  task: '8',
+  description: 'Task description',
+}, {
+  key: '9',
+  name: 'Olecsiy Andriychuk',
+  age: 18,
+  address: 'Zhytomir',
+  task: '9',
+  description: 'Task description',
+}];
+
+export const columns = [{
+  title: 'Name',
+  dataIndex: 'name',
+  key: 'name'
+}, {
+  title: 'Age',
+  dataIndex: 'age',
+  key: 'age',
+}, {
+  title: 'Address',
+  dataIndex: 'address',
+  key: 'address',
+}, {
+  title: 'Task',
+  dataIndex: 'task',
+  key: 'task',
+}, {
+  title: 'Description',
+  dataIndex: 'description',
+  key: 'description',
+}];
