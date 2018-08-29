3bf38f4d7eec031ea4822c972aef4abee5a871e0
Alex-135-135 <olecsij-andrijchuk@ukr.net>
2018-08-29 16:55:55 +0300

    weather

--- weather/app/api/api.component.js
+++ weather/app/api/api.component.js
@@ -4,6 +4,7 @@
 let lat, lon;
 let tempUnit = 'C';
 let currentTempInCelsius;
+let data = [];
 
 angular.
   module('api').
@@ -12,31 +13,29 @@ angular.
     controller: ['Phone',
       function PhoneListController() { }]
   })
-  .controller('deleteFromBasket', $scope => {
+  .controller('deleteFromBasket', ($scope, $http) => {
       if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function (position) {
           let lat = "lat=" + position.coords.latitude;
           let lon = "lon=" + position.coords.longitude;
-          getWeather(lat, lon);
-          console.log(lat + "Component" +lon);
+           $scope.getWeather(lat, lon);
         });
       } else {
         console.log("Geolocation is not supported by this browser.");
       }
 
-function getWeather(lat, lon) {
-  let urlString = "http://api.openweathermap.org/data/2.5/weather?cnt=5&" + lat + "&"+ lon + "&units=metric&APPID=236612a7763329b46b2c67a6cb5f6455";
-  $.ajax({
-    url: urlString, success: function (result) {
-      $("#city").text(result.name + ", ");
-      $("#country").text(result.sys.country);
-      currentTempInCelsius = Math.round(result.main.temp * 10) / 10;
-      $("#temp").text(currentTempInCelsius + " " + String.fromCharCode(176));
-      $("#tempunit").text(tempUnit);
-      console.log(result);
-    }
+    $scope.getWeather = (lat, lon) => {
+    let urlString = "http://api.openweathermap.org/data/2.5/weather?cnt=5&" + lat + "&"+ lon + "&units=metric&APPID=236612a7763329b46b2c67a6cb5f6455";
+        $http({method: 'GET', url: urlString}).
+              then(function success(response) {
+                  $scope.city = (response.data.name + ", ");
+                  $scope.country = response.data.sys.country;
+                  currentTempInCelsius = Math.round(response.data.main.temp * 10) / 10;
+                  $scope.temp = currentTempInCelsius + " " + String.fromCharCode(176);
+                  $scope.tempunit = tempUnit;
+                  $scope.desc = "//openweathermap.org/img/w/"+response.data.weather[0].icon+".png"
+                  console.log(response.data);
+          });
+  }
   });
-}
-
-  });
-
+ 
